cb39f3be7c168bdcbc17281a7cf675fe41ac8a2e
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2019-03-13 19:30:44 -0700

    loading view in html/css, js functions to show/hide it

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -1,7 +1,19 @@
 export function showWelcome() {
-  document.querySelector('main').style.display = 'block';
+  document.getElementById('main').style.display = 'block';
+  document.getElementById('welcome-container').style.display = 'block';
+  document.getElementById('loading').style.display = 'none';
 }
 
 export function hideWelcome() {
-  document.querySelector('main').style.display = 'none';
+  document.getElementById('main').style.display = 'none';
+}
+
+export function showLoading() {
+  document.getElementById('main').style.display = 'block';
+  document.getElementById('welcome-container').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+}
+
+export function hideLoading() {
+  document.getElementById('main').style.display = 'none';
 }
