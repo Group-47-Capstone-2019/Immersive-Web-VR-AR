f5b46a5e8bf5e6cb9f4fb6e6ca729b9e6c834c09
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2019-04-05 16:19:32 -0700

    use textures instead of gltf for planets

--- src/scripts/scenes/planets/index.js
+++ src/scripts/scenes/planets/index.js
@@ -1,11 +1,18 @@
 import {
   AmbientLight,
   PointLight,
-  Color
+  Color,
+  SphereGeometry,
+  MeshPhongMaterial,
+  MeshBasicMaterial,
+  Mesh,
+  RingGeometry,
+  DoubleSide
 } from 'three';
 import XrScene from '../xr-scene';
 import { createPlanets } from './create';
 import { movePlanets } from './orbit';
+import planetData from './planets';
 
 export default class PlanetsScene extends XrScene {
   /**
@@ -16,24 +23,78 @@ export default class PlanetsScene extends XrScene {
   constructor(renderer, camera) {
     super(renderer, camera);
 
-    this.scene.background = new Color('black');
-    this.planets = createPlanets();
-    this.planets.forEach(p => this.scene.add(p.mesh));
+    Object.keys(planetData).forEach(planet =>
+      this.loader.addTextureToQueue(planetData[planet].texture, planet)
+    );
+
+    this.loader.addTextureToQueue(
+      planetData.Saturn.ringsTexture,
+      'rings-texture'
+    );
+
+    // this.planets = createPlanets();
+    // this.planets.forEach(p => this.scene.add(p.mesh));
     this.addLighting();
   }
 
   addLighting() {
-    const ambientLight = new AmbientLight('white', 0.1);
+    const ambientLight = new AmbientLight('white', 0.3);
     this.scene.add(ambientLight);
-
-    const pointLight = new PointLight('white', 0.8, 1000);
-    pointLight.position.set(5, 5, 5);
-    this.scene.add(pointLight);
   }
 
   onAssetsLoaded(cache) {
     super.onAssetsLoaded(cache);
-    return cache;
+
+    this.planets = [];
+    let i = 0;
+
+    Object.keys(planetData).forEach(planetName => {
+      const texture = cache[planetName];
+      const geo = new SphereGeometry(5, 20, 20);
+
+      let material;
+      if (planetName === 'Sun') {
+        material = new MeshBasicMaterial({ map: texture });
+      } else {
+        material = new MeshPhongMaterial({ map: texture });
+      }
+
+      const mesh = new Mesh(geo, material);
+
+      mesh.position.x = i++ * 20 - 80;
+      mesh.position.y = 2;
+      mesh.name = planetName;
+      this.scene.add(mesh);
+      this.planets.push(mesh);
+    });
+
+    const saturn = this.scene.getObjectByName('Saturn');
+    const saturnRingGeo = new RingGeometry(6, 9, 50);
+    const saturnRingMat = new MeshPhongMaterial({
+      map: cache['rings-texture'],
+      side: DoubleSide,
+      transparent: true,
+      opacity: 0.7
+    });
+    const saturnRingMesh = new Mesh(saturnRingGeo, saturnRingMat);
+    saturnRingMesh.rotateX(Math.PI / 2);
+    saturn.add(saturnRingMesh);
+
+    const uranus = this.scene.getObjectByName('Uranus');
+    const uranusRingGeo = new RingGeometry(6, 9, 50);
+    const uranusRingMat = new MeshPhongMaterial({
+      map: cache['rings-texture'],
+      side: DoubleSide,
+      transparent: true,
+      opacity: 0.5
+    });
+    const uranusRingMesh = new Mesh(uranusRingGeo, uranusRingMat);
+    uranusRingMesh.rotateX(Math.PI / 2);
+    uranus.add(uranusRingMesh);
+
+    const sun = this.scene.getObjectByName('Sun');
+    const pointLight = new PointLight('white', 0.8, 1000);
+    sun.add(pointLight);
   }
 
   /**
@@ -42,6 +103,6 @@ export default class PlanetsScene extends XrScene {
    * @param {number} delta
    */
   animate(delta) {
-    movePlanets(this.planets, delta);
+    // movePlanets(this.planets, delta);
   }
 }
---
+++ src/scripts/scenes/planets/planets.js
@@ -0,0 +1,32 @@
+export default {
+  Sun: {
+    texture: require('../../../assets/planets/sunmap.jpg')
+  },
+  Mercury: {
+    texture: require('../../../assets/planets/mercurymap.jpg')
+  },
+  Venus: {
+    texture: require('../../../assets/planets/venusmap.jpg')
+  },
+  Earth: {
+    texture: require('../../../assets/planets/earthmap.jpg')
+  },
+  Mars: {
+    texture: require('../../../assets/planets/marsmap.jpg')
+  },
+  Jupiter: {
+    texture: require('../../../assets/planets/jupitermap.jpg')
+  },
+  Saturn: {
+    texture: require('../../../assets/planets/saturnmap.jpg'),
+    ringsTexture: require('../../../assets/planets/saturnRings.jpg')
+  },
+  Uranus: {
+    texture: require('../../../assets/planets/uranusmap.jpg'),
+    ringsTexture: require('../../../assets/planets/uranusringcolour.jpg'),
+    ringsAlpha: require('../../../assets/planets/uranusringtrans.gif')
+  },
+  Neptune: {
+    texture: require('../../../assets/planets/neptunemap.jpg')
+  }
+};
